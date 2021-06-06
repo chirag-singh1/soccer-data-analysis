b8a7dabd75648a53e585ec49a63d26b2e946cee0
chirag-singh1 <66097224+chirag-singh1@users.noreply.github.com>
2021-06-06 10:36:03 -0700

    Modularized halftime analysis implementation

--- lead-analysis/halftime.py
+++ lead-analysis/halftime.py
@@ -1,19 +1,7 @@
-import json
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import numpy as np
-
-#Helper class to store each goal's set of of information
-class Goal:
-    def __init__(self,teamId,period,time): #Initialize goal
-        self.team=teamId
-        self.period=period
-        self.time=time
-    
-    def update_prev_goals(self,team_id1,team_id2,team1_goals,team2_goals): #Update goal with the score before the goal occurred
-        self.prev_goals={}
-        self.prev_goals[team_id1]=team1_goals
-        self.prev_goals[team_id2]=team2_goals
+import util
     
 #Helper method to update halftime counts 
 def update_counts(goal_record, team_1, team_2, half_time_lead_count,half_time_leading_team_win_count,half_time_draw_count,half_time_leading_team_loss_count, winner):
@@ -39,37 +27,9 @@ def update_counts(goal_record, team_1, team_2, half_time_lead_count,half_time_le
 
 #Main method to parse the goals/match results of a specific competition
 def parse_match_results(competition):
-    events = open(f'../events_line_breaks/events_{competition}.json','r')   
-    matches_file = open(f'../matches_line_breaks/matches_{competition}.json','r')
+    matches=util.parse_matches(competition)
+    goals=util.parse_goals(competition, matches)
     
-    goals=defaultdict(list)
-    matches={}
-
-    #Initialize matches (including competiting teams and the winner)
-    cl=matches_file.readline()
-    while(cl):
-        curr_line=json.loads(cl)
-
-        matches[curr_line['wyId']]={}
-        matches[curr_line['wyId']]['teams']=list(curr_line['teamsData'].keys())
-        matches[curr_line['wyId']]['winner']=curr_line['winner']
-        
-        cl=matches_file.readline()
-
-    #Store each goal in the correct match
-    cl=events.readline()
-    while(cl):
-        curr_line=json.loads(cl)
-
-        if curr_line['eventId'] != 9: #Exclude save attempts that have goal tag
-            if sum(1 for x in curr_line['tags']  if x['id']==101) > 0: #Normal goal
-                goals[curr_line['matchId']].append(Goal(curr_line['teamId'],curr_line['matchPeriod'],curr_line['eventSec']))
-            elif sum(1 for x in curr_line['tags']  if x['id']==102) > 0: #Own goal (swap team)
-                team=int(next(x for x in matches[curr_line['matchId']]['teams'] if x != str(curr_line['teamId'])))
-                goals[curr_line['matchId']].append(Goal(team,curr_line['matchPeriod'],curr_line['eventSec']))
-
-        cl=events.readline()
-
     #Initialize metrics for halftime deficit
     half_time_leading_team_win_count=[0]*10
     half_time_leading_team_loss_count=[0]*10
--- lead-analysis/lead-analysis.py
+++ lead-analysis/lead-analysis.py
@@ -1,11 +1,12 @@
 import argparse
 import halftime
+import nextgoal
 
 if __name__ == '__main__':
 
     #Setup argument parsing
     parser=argparse.ArgumentParser(description='Generate a variety of graphics centered around the halftime lead of specific games')
-    parser.add_argument('-A','--percentage_adjust',required=False,nargs=1, default='False', choices=['True','False'])
+    parser.add_argument('-A','--analysis',required=False,nargs=1, default='halftime-lead', choices=['halftime-lead','halftime-percent-adjusted'])
     parser.add_argument('-C','--competitions',required=False,nargs='*',default='all',
     choices=['Spain', 'England', 'France','Germany','Italy','World_Cup','European_Championship'])
     args = parser.parse_args()
@@ -16,4 +17,10 @@ if __name__ == '__main__':
     else:
         competitions=args.competitions
     
-    halftime.run_analysis(competitions,args.percentage_adjust[0])
+    #Run corresponding analysis
+    if args.analysis[0] == 'halftime-lead':
+        halftime.run_analysis(competitions,'False')
+    elif args.analysis[0] == 'halftime-percent-adjusted':
+        halftime.run_analysis(competitions,'True')
+    elif args.analysis[0] == 'next-goal':
+        nextgoal.run_analysis(competitions)
